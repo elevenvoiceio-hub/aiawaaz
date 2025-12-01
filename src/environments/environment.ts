be750eff951603f5c1cb2714b7f86c09868b6873
mohitmadras <69849430+mohitmadras@users.noreply.github.com>
2025-12-01 15:10:13 +0530

    Update appDomain URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: 'https://django-backend-741299369703.asia-south1.run.app/api',
   applicationName: 'Eleven Voice',
-  appDomain: 'https://aiawaaz-a87bd.web.app',
+  appDomain: 'https://app.elevenvoice.io',
 };
